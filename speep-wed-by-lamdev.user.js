c8523c304b6b26b1eac70633a0fde1e6e2de188a
Lê Anh Minh <lamdev2104@gmail.com>
2025-07-04 18:23:11 +0700

    Create speep-wed-by-lamdev.user.js

---
+++ speep-wed-by-lamdev.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @author       xGreen
+// @require      https://raw.githubusercontent.com/phantatdung233/bypass-yeumoney/refs/heads/main/bypass_yeumoney.js
+// @name         Speep Wed By LAMDev
+// @namespace    http://tampermonkey.net/
+// @version      2.3
+// @description  Menu quản lý script tăng tốc timeout - Không dùng SweetAlert2
+// @author       Hades
+// @match        *://*/*
+// @grant        none
+// @icon         https://i.pinimg.com/236x/26/fc/2f/26fc2faf0e4a6628341e1a6f975654e7.jpg
+// ==/UserScript==
